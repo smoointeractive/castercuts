7558495bc5315ae0005c56400382b0151b333611
Sacha Moo <sacha@smoointeractive.com>
2017-06-01 10:41:40 -0700

    Remove all directories in this branch unrelated to GalleryTwoApp.

--- GWTProjects/gwtsample/target/gwt/devmode/work/pl.czak.gwtsample.SampleApp/compile-2/gen/com/google/gwt/lang/pl_00046czak_00046gwtsample_00046SampleApp__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class pl_00046czak_00046gwtsample_00046SampleApp__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
